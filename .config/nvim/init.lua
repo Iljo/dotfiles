baf498f58918ce2cca407a869510980298a523b0
Iljo <iljo.opposite@gmail.com>
2023-05-03 02:29:33 +0200

    Finalizing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,24 @@
 -- LSP
 
+local git_dirname = vim.fs.dirname(vim.fs.find({'.git'}, { upward = true })[1]) 
 local lspconfig = require('lspconfig')
+
 lspconfig.ltex.setup {
-   settings = {
+    settings = {
         ltex = {
             language = "en-US"
         }
     },
+    on_attach = function(client, bufnr) 
+        require("ltex_extra").setup {
+            load_langs = { "en-US" },
+            path = (git_dirname or "~") .. "/.spell"
+        }
+    end
 }
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         print("Attached buffer", args.buf, "to LSP client", args.data.client_id)
-        require("ltex_extra").setup {
-            load_langs = { "en-US" },
-            path = vim.fs.dirname(vim.fs.find({'.git'}, { upward = true })[1]) .. "/.spell"
-        }
     end
 })
 
